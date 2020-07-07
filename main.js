c098b924cf08b841232e94264bd97d7460183834
kpk2169 <kpk2169@gmail.com>
2020-07-07 09:58:33 +0900

    javascript home transparent

--- main.js
+++ main.js
@@ -32,6 +32,13 @@ homeContactBtn.addEventListener('click', () => {
   scrollIntoView('#contact');
 });
 
+//make home slowly fade to transparent as the window scrolls
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({ behavior: 'smooth' });
